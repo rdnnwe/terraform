92419f32aebf2fee86129434254d67e88d3dc313
rdnnwe <54474328+rdnnwe@users.noreply.github.com>
2020-07-08 17:37:01 +0300

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+    source = "github.com/terraform-aws-modules/terraform-aws-vpc"
+    
+    name = "test-vpc"
+    cidr = "10.1.0.0/16"
+    azs  = ["eu-west-1a", "eu-west-1b"]
+    private_subnets = ["10.1.0.0/18", "10.1.128.0/18"]
+    public_subnets  = ["10.1.64.0/18", "10.1.192.0/18"]
+    
+    enable_nat_gateway = true
+    enable_dns_support = true
+
+    tags = {
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
