ed57c2f00a6782b045f083e9e691616d05ab2a82
rdnnwe <54474328+rdnnwe@users.noreply.github.com>
2020-07-14 02:14:03 +0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ module "vpc" {
     azs  = ["eu-west-1a", "eu-west-1b"]
     private_subnets = ["10.1.0.0/18", "10.1.128.0/18"]
     public_subnets  = ["10.1.64.0/18", "10.1.192.0/18"]
-    
+        
     enable_nat_gateway = true
     enable_dns_support = true
 
